06699ee2d4cb14534c237eaccebd65a72fc54b8c
harsh08-rgb <hurshsoni83401@gmail.com>
2026-04-24 09:26:48 +0530

    UC1

---
+++ Usecase/src/trainmanagment.java
@@ -0,0 +1,12 @@
+public class trainmanagment {
+}
+    public static void main(String[] args) {
+        System.out.println("==================");
+        System.out.println("==Train Consist Management APP==");
+        System.out.println("==================");
+        System.out.println("Train Initialised Successfully");
+        System.out.println("Initial bogie count : 0");
+        System.out.println("Current Train Consist : []");
+        System.out.println("Systems ready for operations");
+    }
+}
