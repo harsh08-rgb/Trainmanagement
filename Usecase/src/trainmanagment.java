ddce5a9962e6b3f7ce159a67a9c20f54a7a861d7
harsh08-rgb <hurshsoni83401@gmail.com>
2026-04-24 10:08:32 +0530

    UC10

--- Usecase/src/trainmanagment.java
+++ Usecase/src/trainmanagment.java
@@ -2,7 +2,6 @@ public class trainmanagment {
 }
 
 
-
 // Reusing Bogie model
 static class Bogie {
     String name;
@@ -17,7 +16,7 @@ static class Bogie {
 public static void main(String[] args) {
 
     System.out.println("======================================");
-    System.out.println("UC9 - Group Bogies by Type");
+    System.out.println("UC10 - Count Total Seats in Train");
     System.out.println("======================================\n");
 
     // Create list of bogies
@@ -27,31 +26,21 @@ public static void main(String[] args) {
     bogies.add(new Bogie("AC Chair", 56));
     bogies.add(new Bogie("First Class", 24));
     bogies.add(new Bogie("Sleeper", 70));
-    bogies.add(new Bogie("AC Chair", 60));
 
-    // Display all bogies
-    System.out.println("All Bogies:");
+    // Display bogies
+    System.out.println("Bogies in Train:");
     for (Bogie b : bogies) {
         System.out.println(b.name + " -> " + b.capacity);
     }
 
-    // Group using Collectors.groupingBy()
-    Map<String, List<Bogie>> groupedBogies =
-            bogies.stream()
-                    .collect(Collectors.groupingBy(b -> b.name));
+    // Aggregate using map + reduce
+    int totalSeats = bogies.stream()
+            .map(b -> b.capacity)     // extract capacity
+            .reduce(0, Integer::sum); // sum all values
 
-    // Display grouped structure
-    System.out.println("\nGrouped Bogies:");
+    // Display total
+    System.out.println("\nTotal Seating Capacity of Train: " + totalSeats);
 
-    for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-
-        System.out.println("\nBogie Type: " + entry.getKey());
-
-        for (Bogie b : entry.getValue()) {
-            System.out.println("Capacity -> " + b.capacity);
-        }
-    }
-
-    System.out.println("\nUC9 grouping completed...");
+    System.out.println("\nUC10 aggregation completed...");
 }
 }
